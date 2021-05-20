c6b3e6d1690ba0eae6e3a15879d39aa175d9c3de
Suraj <ogsurajchand@gmail.com>
2021-05-20 14:55:25 +0545

    Testing Done

--- IrisData.py
+++ IrisData.py
@@ -29,3 +29,33 @@ KNN.fit(x,y)
 
 x_new = np.array([[5.6,3.4,1.4,0.1]])
 val = KNN.predict(x_new)
+
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,train_size=0.8,
+                                                 random_state=88,shuffle=True,stratify=y)
+
+from sklearn.neighbors import KNeighborsClassifier
+ 
+KNN = KNeighborsClassifier(n_neighbors=6,metric='minkowski',p=1)
+KNN.fit(x_train,y_train)
+
+predicted = KNN.predict(x_test)
+
+from sklearn import metrics
+
+metrics.accuracy_score(y_test,predicted)
+
+
+
+
+
+
+
+
+
+
+
+
+
